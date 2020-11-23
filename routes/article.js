397954ea7af9d665f54a3bc78693abcabdcedd7f
PulkitJoshi <email.pulkitj@gmail.com>
2020-11-24 03:59:35 +0530

    check

--- routes/article.js
+++ routes/article.js
@@ -1,10 +1,25 @@
 const router = require('express').Router();
-const verify = require('./verifyToken');
+const verify = require('./Verification/verifyUser');
 const Article = require('../model/Article');
 
-router.post('/article/add', verify, async (req, res) => {
+router.post('/article/publish', verify, async (req, res) => {
     
 })
 
+router.get('/article/:id', verify, async (req, res) => {
+    
+})
+
+router.post('/article/modify/:id', verify, async (req, res) => {
+    
+})
+
+router.get('/article/modify/:id', verify, async (req, res) => {
+    
+})
+
+router.post('/article/remove/:id', verify, async (req, res) => {
+    
+})
 
 module.exports = router;
